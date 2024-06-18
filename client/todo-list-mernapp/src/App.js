f87b9c329e184617730d61dd11facde76fafab29
CTHartze <ismailmaho007@gmail.com>
2024-06-18 13:37:41 +0200

    Route creation, adding and fetching items from database

--- client/todo-list-mernapp/src/App.js
+++ client/todo-list-mernapp/src/App.js
@@ -1,11 +1,25 @@
+import {useState} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [itemText, setItemText] = useState('')
+
+  //add new item to database
+  const addItem = async () => {
+    try{
+      const res = await axios.post('http://localhost:5500/api/item', {item: itemText})
+      console.log(res);
+    }catch(err){
+      console.log(err);
+    }
+  }
+
   return (
     <div className="App">
       <h1>Task Master</h1>
-      <form className="form">
-        <input type="text" placeholder='Add Task Item' />
+      <form className="form" onSubmit={()}>
+        <input type="text" placeholder='Add Task Item' onChange={e => {setItemText(e.target.value)} } value={itemText}/>
         <button type="submit">Add</button>
       </form>
       <div className="todo-listItems">
